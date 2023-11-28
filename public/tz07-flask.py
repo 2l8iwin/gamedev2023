54275751c937489c85392c432e60aac0ff640a09
2l8iwin <82132425+2l8iwin@users.noreply.github.com>
2023-11-28 13:28:22 -0800

    Create tz07-flask.py

---
+++ public/tz07-flask.py
@@ -0,0 +1,10 @@
+## file is called tz07-flask.py
+
+from flask inport flask, request, redirect, url_for
+inport subprocess
+
+app = flask(___name__)
+
+@app.route('/')
+def home()
+return redirect(url
